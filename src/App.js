f0551eb7db4443bb8b2def497d07afb4b53814b7
agata okolska <agata.okolska@gmail.com>
2018-08-10 13:34:29 +0200

    counter increase, decrease and reset numbers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import Counter from './Counter'
 
 
 class App extends React.Component {
   render() {
     return (
       <div>
-       
+       <Counter 
+       />
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React from 'react'
 
 
 
 class Counter extends React.Component {
 
     state = {
-        number: null
+        number: 0
     }
 
+    increaseNumberHandler = () => {
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
 
+    decreaseNumberHandler = () => {
+        this.setState({
+            number: this.state.number - 1
+        })
+    }
 
-  render() {
-    return (
-      <div>
-       
-      </div>
-    );
-  }
+    resetNumberHandler = () => {
+        this.setState({
+            number: 0
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.number}
+                </h1>
+                <button
+                    onClick={this.increaseNumberHandler}
+                >
+                    +
+                </button>
+                <button
+                    onClick={this.decreaseNumberHandler}
+                >
+                    -
+                </button>
+
+                <button
+                    onClick={this.resetNumberHandler}
+                >
+                    RESET
+                </button>
+            </div>
+        )
+    }
 }
 
 export default Counter
