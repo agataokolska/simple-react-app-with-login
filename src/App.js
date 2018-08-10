55b20ddf00e4a6d37e38fad48051bbaf6a519f69
agata okolska <agata.okolska@gmail.com>
2018-08-10 13:04:27 +0200

    add Counter component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div>
---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+class Counter extends React.Component {
+
+    state = {
+        number: null
+    }
+
+
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
+
+export default Counter
